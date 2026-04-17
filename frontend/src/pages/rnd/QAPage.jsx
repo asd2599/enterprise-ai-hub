fb204e707e3dcf738f82d2f09118600e713cb66c
asd2599 <asd2590@naver.com>
2026-04-17 15:13:39 +0900

    버그 수정

--- frontend/src/pages/rnd/QAPage.jsx
+++
@@ -1,6 +0,0 @@
-// QA/품질관리팀 페이지
-import DeptPageLayout from '../../components/layout/DeptPageLayout'
-
-export default function QAPage() {
-  return <DeptPageLayout categoryId="rnd" deptId="qa" />
-}
